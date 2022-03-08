5389b0cf0120c21e29b0505f402be6bb3235863e
unknown <yusyaepul@gmail.com>
2022-03-08 20:50:31 +0700

    Tugas Hari ke 2

---
+++ Tugas-2-TypeData/type.dart
@@ -0,0 +1,65 @@
+// void main(){
+// Soal No 1
+// var word = 'dart';
+// var second = 'is';
+// var third = 'awesome';
+// var fourth = 'and';
+// var fifth = 'I';
+// var sixth = 'love';
+// var seventh = 'it!';
+
+// print(word+' '+second+' '+third+' '+fourth+' '+fifth+' '+sixth+' '+seventh);
+// Batas Soal No 1
+// }
+//void main(){
+// Soal No 2
+// var sentence = "I am going to be Flutter Developer";
+
+// var firstWord = sentence[0] ;
+// var secondWord = sentence[2] + sentence[3] ;
+// var thirdWord = sentence[5] + sentence[6] + sentence[7] + sentence[8] + sentence[9]; 
+// var fourthWord = sentence[11] + sentence[12]; 
+// var fifthWord = sentence[14] + sentence[15]; 
+// var sixthWord = sentence[17] + sentence[18] + sentence[19] + sentence[20] + sentence[21] + sentence[22] + sentence[23]; 
+// var seventhWord = sentence[25] + sentence[26] + sentence[27] + sentence[28] + sentence[29] + sentence[30] + sentence[31] + sentence[32] + sentence[33]; 
+
+// print('First Word: ' + firstWord);
+// print('Second Word: ' + secondWord);
+// print('Third Word: ' + thirdWord);
+// print('Fourth Word: ' + fourthWord);
+// print('Fifth Word: ' + fifthWord);
+// print('Sixth Word: ' + sixthWord);
+// print('Seventh Word: ' + seventhWord);
+// Batas Soal No 2
+
+//}
+
+// Soal No 3
+// import 'dart:io';
+// void main(List<String> args) {
+//   print("Masukkan Nama Depan");
+//   String inputNamaDepan = stdin.readLineSync()!;
+//   print("Masukkan Nama Belakang");
+//   String inputNamaBelakang = stdin.readLineSync()!;
+//   print("Nama Lengkap : ${inputNamaDepan} ${inputNamaBelakang}");
+// }
+// Batas Soal No 3
+
+// Soal No 4
+// void main(){
+//   var a = 5;
+//   var b = 10;
+
+//   var perkalian = a*b;
+//   var pembagian = a/b;
+//   var penambahan = a+b;
+//   var pengurangan = a-b;
+  
+
+//   print('Perkalian : ${perkalian}');
+//   print('Pembagian : ${pembagian}');
+//   print('Penambahan : ${penambahan}');
+//   print('Pengurangan : ${pengurangan}');
+  
+// }
+// Batas Soal No 4
