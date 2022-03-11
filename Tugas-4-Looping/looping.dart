5025b18ba0b4db53bcf9ddd0e20fe1a4515163c0
unknown <yusyaepul@gmail.com>
2022-03-11 18:58:25 +0700

    Pengumpulan Tugas Hari ke 4

---
+++ Tugas-4-Looping/looping.dart
@@ -0,0 +1,60 @@
+// Jawaban untuk Nomor 1
+// void main(){
+//     var a = 2;
+//     print('LOOPING PERTAMA');
+//     while (a < 22){
+//         print('${a} - I love coding');
+//         a = a + 2;
+//     }
+
+//     print('LOOPING KEDUA');
+//     var b = 20;
+//     while (b > 0){
+//         print('${b} - I will become a mobile developer');
+//         b -= 2;
+//     }
+// }
+
+// Jawaban untuk Nomor 2
+// void main(){
+//     print('OUTPUT');
+//     for (var i = 1; i <= 20; i++) {
+//     var output = '';
+//     if (i % 2 == 1) {
+//       if (i % 3 == 0) {
+//         output = '${i} - I Love Coding';
+//       } else {
+//         output = '${i} - Santai';
+//       }
+//     } else {
+//       output = '${i} - Berkualitas';
+//     }
+//     print(output);
+//   }
+// }
+
+// Jawaban untuk Nomor 3
+// void main(){
+//   var pjg = 8;
+//   var lbr = 4;
+
+//   for (var i = 0; i < lbr; i++) {
+//     var baris = '';
+//     for (var j = 0; j < pjg; j++) {
+//       baris += '#';
+//     }
+//     print(baris);
+//   }
+// }
+
+// Jawaban untuk Nomor 4
+// void main(){
+//   var sisi = 7;
+//   for (var i = 0; i < sisi; i++) {
+//     var baris = '';
+//     for (var j = 0; j < i + 1; j++) {
+//       baris += '#';
+//     }
+//     print(baris);
+//   }
+// }
