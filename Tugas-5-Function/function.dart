80f3fcb0122df41b89837c3aeaf1395f41b0c9bc
unknown <yusyaepul@gmail.com>
2022-03-11 19:40:54 +0700

    Pengumpulan Tugas Hari ke 5

---
+++ Tugas-5-Function/function.dart
@@ -0,0 +1,54 @@
+// Jawaban untuk Nomor 1
+// void main(){
+//   teriak();
+// }
+
+// teriak(){
+//   print('Halo Sanbers!');
+// }
+
+// Jawaban untuk Nomor 2
+// void main(){
+//   var num1 = 12;
+//   var num2 = 4;
+
+//   var hasilKali = kalikan(num1, num2);
+//   print(hasilKali);
+// }
+
+// kalikan(num1, num2){
+//   return num1 * num2;
+// }
+
+// Jawaban untuk Nomor 3
+// void main(){
+//   var name = 'Agus';
+//   var age = 30;
+//   var address = 'Jln. Malioboro, Yogyakarta';
+//   var hobby = 'Gaming';
+
+//   var perkenalan = introduce(name, age, address, hobby);
+//   print(perkenalan);
+// }
+
+// introduce(name, age, address, hobby){
+//   return 'Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}!';
+// }
+
+// Jawaban untuk Nomor 4
+// void main(){
+//   var nilai = 5;
+//   print(faktorial(nilai));
+// }
+
+// faktorial(nilai){
+//   var angka = 1;
+//   var faktor = 1;
+
+//   while(angka <= nilai){
+//     faktor = faktor * angka;
+//     angka = angka + 1;
+//   }
+
+//   return '${faktor} adalah faktorial dari ${nilai}';
+// }
