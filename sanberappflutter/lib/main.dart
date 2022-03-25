4e4dcd706c11a09a30a2b6683ac9ff8a437d5432
unknown <yusyaepul@gmail.com>
2022-03-26 00:33:23 +0700

    Pengumpulan Tugas ke 15

--- sanberappflutter/lib/main.dart
+++ sanberappflutter/lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:sanberappflutter/Tugas/Tugas11/Telegram.dart';
-import 'package:sanberappflutter/Tugas/Tugas13/HomeScreen.dart';
-import 'package:sanberappflutter/Tugas/Tugas13/LoginScreen.dart';
-import 'package:sanberappflutter/Tugas14/get_data.dart';
+//import 'package:sanberappflutter/Tugas/Tugas11/Telegram.dart';
+//import 'package:sanberappflutter/Tugas/Tugas13/HomeScreen.dart';
+//import 'package:sanberappflutter/Tugas/Tugas13/LoginScreen.dart';
+//import 'package:sanberappflutter/Tugas/Tugas14/get_data.dart';
+import 'package:sanberappflutter/Tugas/Tugas15/loginscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: GetDataScreen(),
+      home: LoginScreen(),
     );
   }
 }
